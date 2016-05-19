e31a7e0e8eae93d516d7e6e29ae47e74e7d5e497
a_voyanets <a.voyanets@gmail.com>
2016-05-19 12:44:50 +0300

    test 1

---
+++ Test.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: a_voyanets
+ * Date: 5/19/16
+ * Time: 12:43 PM
+ */
+
+namespace Test;
+
+class Test
+{
+
+    public function __construct()
+    {
+
+    }
+
+}
